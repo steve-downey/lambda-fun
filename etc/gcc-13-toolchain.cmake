9c319f4c83b17f97dbcd64e5c8ebecceeb554927
Steve Downey <sdowney@gmail.com>
2024-06-08 14:49:47 -0400

    Update project config
    Better CI, docs, current toolchains, etc.

--- etc/gcc-13-toolchain.cmake
+++ etc/gcc-13-toolchain.cmake
@@ -2,5 +2,5 @@ include_guard(GLOBAL)
 
 include("${CMAKE_CURRENT_LIST_DIR}/gcc-flags.cmake")
 
-set(CMAKE_C_COMPILER cc)
-set(CMAKE_CXX_COMPILER c++)
+set(CMAKE_C_COMPILER gcc-13)
+set(CMAKE_CXX_COMPILER g++-13)
--- etc/llvm-16-toolchain.cmake
+++ etc/llvm-16-toolchain.cmake
@@ -1,12 +1,12 @@
 include_guard(GLOBAL)
 
-set(CMAKE_C_COMPILER clang-14)
-set(CMAKE_CXX_COMPILER clang++-14)
+set(CMAKE_C_COMPILER clang-16)
+set(CMAKE_CXX_COMPILER clang++-16)
 
 set(CMAKE_CXX_FLAGS
   "-std=c++20 \
    -Wall -Wextra \
-   -stdlib=libstdc++ "
+   -stdlib=libc++ -fexperimental-library"
 CACHE STRING "CXX_FLAGS" FORCE)
 
 set(CMAKE_CXX_FLAGS_DEBUG "-O0 -fno-inline -g3" CACHE STRING "C++ DEBUG Flags" FORCE)
